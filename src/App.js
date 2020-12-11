3ec23fd2f93060828534c98612f62a030bd9cb29
Claire Lesh <cteterita@gmail.com>
2020-12-11 14:38:23 -0800

    basic app setup

--- src/App.js
+++ src/App.js
@@ -2,8 +2,16 @@ import React from 'react';
 
 function App() {
   return (
-    <main className='App'>
-      {/* content goes here */}
+    <main className="app">
+      <header>
+        <h1>On Location</h1>
+      </header>
+      <div>
+        {/* content goes here */}
+      </div>
+      <footer>
+        (c) 2020 Claire Teter Lesh - About - Send Feedback
+      </footer>
     </main>
   );
 }
