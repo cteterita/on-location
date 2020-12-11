c8c42fea356863753f36f248d25c763eb5034d9b
Claire Lesh <cteterita@gmail.com>
2020-12-11 15:22:24 -0800

    added router for app test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders without errors', () => {
-  render(<App />);
+  render(<BrowserRouter><App /></BrowserRouter>);
   const linkElement = screen.getByText(/on location/i);
   expect(linkElement).toBeInTheDocument();
 });
