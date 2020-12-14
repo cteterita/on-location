d8f1e52f677b27c0b4843471620b5debb9752013
Claire Lesh <cteterita@gmail.com>
2020-12-14 13:57:08 -0800

    simplified results page

--- src/Results/Results.js
+++ src/Results/Results.js
@@ -9,24 +9,7 @@ function Results() {
   return (
     <>
       <Search />
-      <section className="map-result-holder">
-        <ResultMap />
-        <div className="result-holder">
-          <h3>Home Alone 3: Lost in New York</h3>
-          <p>&#9733;&#9733;&#9733;&#9733;&#9734;</p>
-          <div>
-            <p>
-              This is a description of the movie Home Alone 3: Lost in New York.
-            </p>
-            <p>
-              Its about terrible parenting. Full disclosure, I havent actually
-              seen it.
-            </p>
-          </div>
-          <p>Tags: Good for Kids, Christmas</p>
-          <a href="http://www.google.com" target="_blank" rel="noreferrer">Watch it on Netflix</a>
-        </div>
-      </section>
+      <ResultMap />
     </>
 
   );
