fe99725ee7ea12803e2e89b43ad4eb19a2ae3b19
Claire Lesh <cteterita@gmail.com>
2020-12-14 16:03:18 -0800

    replaced Results component

---
+++ src/Results/Results.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Search from '../Search/Search';
+import ResultMap from '../ResultMap/ResultMap';
+
+function Results() {
+  return (
+    <>
+      <Search />
+      <ResultMap />
+    </>
+
+  );
+}
+
+export default Results;
