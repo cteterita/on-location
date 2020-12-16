f20e2b4913b262b44e19ee2d753aa2139a2356b9
Claire Lesh <cteterita@gmail.com>
2020-12-15 18:18:21 -0800

    added button to trigger add marker modal

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import Popup from 'reactjs-popup';
+
 import Search from './Search/Search';
 import ResultMap from './ResultMap/ResultMap';
+
+const addMarkerButton = (
+  <button type="button" className="add-marker">+ Add Marker</button>
+);
 
 function App() {
   const defaultSearch = 'Search for books, movies, and TV shows to take you away to your favorite destinations.';
@@ -13,6 +19,9 @@ function App() {
       <section className="main">
         <Search defaultSearch={defaultSearch} />
         <ResultMap />
+        <Popup trigger={addMarkerButton} position="right center" modal>
+          Popup!
+        </Popup>
       </section>
       <footer>
         (c) 2020 Claire Teter Lesh - About - Send Feedback
